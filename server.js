de01784e7f6999175f99cd8b88dccc85763ca87e
Tautis <69822476+Tautis@users.noreply.github.com>
2023-04-12 16:29:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const data = require('./db.json')
 const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
 
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
+// For mocking the POST and PUT requests, they won't make any changes to the DB in production environment
 const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
     _isFake: isProductionEnv
 })
+
+// Add a PUT endpoint to the router
+router.render = (req, res) => {
+  if (req.method === 'PUT') {
+    res.jsonp({
+      message: 'Data updated successfully',
+    })
+  } else {
+    res.jsonp(res.locals.data)
+  }
+}
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
@@ -18,6 +30,21 @@ server.use((req, res, next) => {
         router.db.setState(clone(data))
     next()
 })
+
+// Add the PUT endpoint to the server
+server.put('/resource/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const resource = router.db.get('resource').find({ id }).value();
+  if (resource) {
+    router.db.get('resource').find({ id }).assign(body).write();
+    res.status(200).jsonp(router.db.get('resource').find({ id }).value());
+  } else {
+    res.status(404).jsonp({
+      message: `Resource with id ${id} not found`,
+    });
+  }
+});
 
 server.use(router)
 server.listen(process.env.PORT || 8000, () => {
