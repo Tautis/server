ea781f07032261cae1bdfd4d19e0ad51d387abe1
Tautis <69822476+Tautis@users.noreply.github.com>
2023-04-12 16:53:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,22 +5,10 @@ const data = require('./db.json')
 const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
 
-// For mocking the POST and PUT requests, they won't make any changes to the DB in production environment
+// For mocking the POST request, POST request won't make any changes to the DB in production environment
 const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
     _isFake: isProductionEnv
 })
-
-// Add a POST endpoint to the router
-router.render = (req, res) => {
-  if (req.method === 'POST') {
-    res.jsonp({
-      message: 'Data added successfully',
-    })
-  } else {
-    res.jsonp(res.locals.data)
-  }
-}
-
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
@@ -30,13 +18,6 @@ server.use((req, res, next) => {
         router.db.setState(clone(data))
     next()
 })
-
-// Add the POST endpoint to the server
-server.post('/resource', (req, res) => {
-  const body = req.body;
-  const resource = router.db.get('resource').insert(body).write();
-  res.status(201).jsonp(resource);
-});
 
 server.use(router)
 server.listen(process.env.PORT || 8000, () => {
