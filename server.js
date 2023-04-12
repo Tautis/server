85a190272b8eeda36c1f804baaf3d8dd9c07e10a
Tautis <69822476+Tautis@users.noreply.github.com>
2023-04-12 17:38:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const clone = require('clone')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
-const adapter = new FileSync('./data/db.json')
+const adapter = new FileSync('./db.json')
 const db = low(adapter)
 
 db.defaults({ positions: [] }).write()
